5f897f898376315c88677d12acd3758b004940a9
Rodrigo Rossi <rodrigo.rossi.trabal@gmail.com>
2016-04-13 01:18:03 -0300

    Listado hecho con razor

--- Web/Controllers/EmployeesController.cs
+++ Web/Controllers/EmployeesController.cs
@@ -13,77 +13,11 @@ namespace Web.Controllers
         {
             return View();
         }
-
-        // GET: Employees/Details/5
-        public ActionResult Details(int id)
+        // GET: Employees/list
+        public ActionResult List()
         {
-            return View();
-        }
-
-        // GET: Employees/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: Employees/Create
-        [HttpPost]
-        public ActionResult Create(FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add insert logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: Employees/Edit/5
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
-
-        // POST: Employees/Edit/5
-        [HttpPost]
-        public ActionResult Edit(int id, FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add update logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: Employees/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
-        // POST: Employees/Delete/5
-        [HttpPost]
-        public ActionResult Delete(int id, FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add delete logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
+            var dl = WebApiConfig.blHandler.GetAllEmployees();
+            return View(dl);
         }
     }
 }
